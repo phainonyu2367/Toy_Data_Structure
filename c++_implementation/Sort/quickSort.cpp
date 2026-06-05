0c4dbc26f7cf184f8926380a8e8086bbf3754b81
phainon2367 <robertyu2024@163.com>
2026-06-05 17:17:36 +0800

    [feat]: implemented quickSort.

---
+++ c++_implementation/Sort/quickSort.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+void partition(std::vector<int> values, int low, int high) {
+    int left, right, hole;
+    while (low < high) {
+        left = low;
+        right = high;
+        hole = values[left];
+        while (left < right) {
+            while (values[right] > hole) {
+                right -= 1;
+            }
+            values[left] = values[right];
+            while (values[left] <= hole) {
+                left += 1;
+            }
+            values[right] = values[left];
+            partition(values, low, left - 1);
+        }
+        values[left] = hole;
+        low += 1;
+    }
+}
+
+void quickSort(std::vector<int> values) {
+    partition(values, 0, values.size() - 1);
+}   
+
